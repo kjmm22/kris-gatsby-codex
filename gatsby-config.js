f2d368540d878f6de17593f4f047ac65ba0b7a66
Marco Pereira <116118992+Markuspg1@users.noreply.github.com>
2025-07-30 13:15:28 -0500

    Initialize Gatsby site for Netlify

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Kris Gatsby Codex',
+    description: 'Sample Gatsby portfolio deployed on Netlify',
+    author: 'Kris'
+  },
+  plugins: ['gatsby-plugin-netlify']
+};
