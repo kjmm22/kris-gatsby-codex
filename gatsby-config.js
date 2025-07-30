0afb8cdf07eaf1adba98abaa1a998b04c5618126
Kristhal Meng <kristhal.meng@gmail.com>
2025-07-30 13:18:51 -0500

    Delete gatsby-config.js

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Kris Gatsby Codex',
-    description: 'Sample Gatsby portfolio deployed on Netlify',
-    author: 'Kris'
-  },
-  plugins: ['gatsby-plugin-netlify']
-};
