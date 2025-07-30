32e701680a51b773340b7fc9d09b9f754bba27d1
Marco Pereira <116118992+Markuspg1@users.noreply.github.com>
2025-07-30 12:46:49 -0500

    Initialize Gatsby site with portfolio

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Portfolio from '../components/Portfolio';
+
+const IndexPage = () => (
+  <main>
+    <h1>Welcome to Kris Gatsby Codex</h1>
+    <Portfolio />
+  </main>
+);
+
+export default IndexPage;
